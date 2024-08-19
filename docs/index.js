e7d909329a846f5c1a67534dddcf62c4bf390822
Miso Kim <meeso.kim@gmail.com>
2024-08-19 22:56:53 +0900

    docs/images/HELI_ADV.tap.png docs/index.js docs/index.wasm docs/software_collection.html docs/taps/HELI_ADV.tap readme.md tools/SPC1000_program_collection.csv

--- docs/index.js
+++ docs/index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp60g8g89c.js
+// include: /tmp/tmpxaabh9k0.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -220,21 +220,21 @@ Module['FS_createPath']("/", "taps2", true, true);
 
   })();
 
-// end include: /tmp/tmp60g8g89c.js
-// include: /tmp/tmpq7o2ics_.js
+// end include: /tmp/tmpxaabh9k0.js
+// include: /tmp/tmpbr17tqgf.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpq7o2ics_.js
-// include: /tmp/tmpjh3k_v0u.js
+  // end include: /tmp/tmpbr17tqgf.js
+// include: /tmp/tmpdk0gcj30.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpjh3k_v0u.js
+  // end include: /tmp/tmpdk0gcj30.js
 
 
 // Sometimes an existing Module object exists with properties
